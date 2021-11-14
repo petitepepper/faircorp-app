52e52236e25903bebe18bcfe41021d8ad8e5669b
petitepepper <meijiajing623@163.com>
2021-11-14 22:25:15 +0100

    add first layout

--- android/app/src/main/java/com/faircorp/MainActivity.kt
+++ android/app/src/main/java/com/faircorp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.faircorp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openWindow(view: View) {
+        // Extract value filled in editext identified with txt_window_name id
+        val windowName = findViewById<EditText>(R.id.txt_window_name).text.toString()
+        // Display a message
+        Toast.makeText(this, "You choose $windowName", Toast.LENGTH_LONG).show()
     }
 }
