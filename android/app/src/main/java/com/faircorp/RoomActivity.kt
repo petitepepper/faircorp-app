00e4c97258cef1c370140331b450e498b5b48abd
petitepepper <meijiajing623@163.com>
2021-11-18 20:50:48 +0100

    Add windows list in RoomActivity.kt

--- android/app/src/main/java/com/faircorp/RoomActivity.kt
+++ android/app/src/main/java/com/faircorp/RoomActivity.kt
@@ -1,16 +1,22 @@
 package com.faircorp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.faircorp.model.ApiServices
+import com.faircorp.model.OnWindowSelectedListener
+import com.faircorp.model.WindowAdapter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class RoomActivity : BasicActivity() {
+class RoomActivity : BasicActivity(), OnWindowSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room)
@@ -38,7 +44,7 @@ class RoomActivity : BasicActivity() {
                                 room.targetTemperature?.toString()
                             findViewById<TextView>(R.id.txt_room_building).text =
                                 room.buildingName
-                            //TODO: Add window list
+                            loadWindows(roomId)
                         }
                     }
                 }
@@ -53,6 +59,44 @@ class RoomActivity : BasicActivity() {
                 }
 
         }
+
+    }
+
+
+    private fun loadWindows(roomId:Long){
+        val recyclerView = findViewById<RecyclerView>(R.id.list_windows_in_room)
+        val adapter = WindowAdapter(this)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+
+        //Find all windows and show information of each
+        lifecycleScope.launch(context = Dispatchers.IO) {
+            //TODO: Should have a "findWindowsInRoom" api service!
+            runCatching { ApiServices().windowsApiService.findAll().execute() }
+                .onSuccess {
+                    withContext(context = Dispatchers.Main) {
+                        adapter.update(it.body() ?: emptyList())
+                    }
+                }
+                .onFailure {
+                    withContext(context = Dispatchers.Main) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Error on windows loading $it",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+
+    }
+
+    override fun onWindowSelected(id: Long) {
+        val intent = Intent(this, WindowActivity::class.java).putExtra(WINDOW_NAME_PARAM, id)
+        startActivity(intent)
     }
 }
 
